b55d477f35e2dffd442e755dc1cba84971c039a7
张泽中 <16664476+zzz1029335886@users.noreply.github.com>
2021-09-30 14:55:26 +0800

    Initial Commit

---
+++ ZZSnapMutipleViewSetup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZZSnapMutipleViewSetup
+//
+//  Created by zerry on 2021/9/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
